ae96ba10e8b398d279c2e26dd2e64d4c77aa49f9
dsh41 <dsh41@INSPIRON>
2024-11-17 11:56:36 -0600

    Add project files.

---
+++ Ingredient.cs
@@ -0,0 +1,12 @@
+﻿
+namespace cookies_cookBook
+{
+    abstract class Ingredients
+    {
+        public abstract string name { get; set;}
+        public abstract int id { get; set; }
+
+        public abstract void Preparation();
+
+    }
+}
